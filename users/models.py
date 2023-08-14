efeb2abf0ae20e5c870a6a58e40b23ffa5f37d92
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2023-08-14 23:55:53 +0300

    fix app users, add new class room

--- users/models.py
+++ users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phone_field import PhoneField
 
-from users.validators import CorrectEmail, CorrectUsernameAndNotMe
+from users.validators import CorrectUsernameAndNotMe
 
 
-class Tag(models.Model):
+class Service(models.Model):
     name_service = models.CharField(
         verbose_name='Название услуги',
         max_length=settings.MAX_LEN_NAME
@@ -15,12 +15,11 @@ class Tag(models.Model):
         verbose_name='Фотография услуги',
         upload_to='users/tags',
         blank=True,
-
     )
     cost_service = models.PositiveIntegerField(
         verbose_name='Стоимость услуги'
     )
-    description_service = models.CharField(
+    description_service = models.TextField(
         verbose_name='Описание услуги',
         max_length=settings.MAX_LEN_NAME
     )
@@ -31,7 +30,6 @@ class Tag(models.Model):
         verbose_name='Оборудование',
         max_length=settings.MAX_LEN_NAME
     )
-#   author_id = models.ForeignKey(User)
 
     class Meta:
         verbose_name = 'Услуга'
@@ -42,8 +40,6 @@ class Tag(models.Model):
 
 
 class User(AbstractUser):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
     first_name = models.CharField(
         verbose_name='Имя',
         max_length=settings.MAX_LEN_NAME,
@@ -65,8 +61,7 @@ class User(AbstractUser):
     )
     email = models.EmailField(
         'Почта',
-        unique=True,
-        validators=[CorrectEmail],
+        unique=True
     )
     phone = PhoneField(
         verbose_name='Номер телефона',
@@ -74,37 +69,36 @@ class User(AbstractUser):
         help_text='Телефон для контакта'
 
     )
-    tag_id = models.ManyToManyField(
-        Tag,
-        verbose_name='Теги',
+    service = models.ManyToManyField(
+        Service,
+        verbose_name='Услуги',
         related_name='users',
     )
     work_experience = models.FloatField(
         verbose_name='Опыт работы',
         default=0,
     )
-
 # Нужно сделать базу с городами
     city = models.CharField(
         verbose_name='Город',
         help_text='Укажите город проживания',
         max_length=settings.MAX_LEN_NAME
     )
-    raiting = models.IntegerField(
+    raiting = models.PositiveSmallIntegerField(
         verbose_name='Рейтинг профиля',
         blank=True,
         null=True,
     )
-    about_me = models.CharField(
+    about_me = models.TextField(
         verbose_name='Обо мне',
         max_length=settings.MAX_TEXT_LEN
     )
     is_photographer = models.BooleanField(
-        verbose_name='Роль. Фотограф',
+        verbose_name='Роль.Фотограф',
         default=False
     )
     is_video_operator = models.BooleanField(
-        verbose_name='Роль. Видео оператор',
+        verbose_name='Роль.Видео-оператор',
         default=False
     )
     birthday = models.DateField(
@@ -112,11 +106,18 @@ class User(AbstractUser):
         blank=True,
         null=True
     )
-    social = models.CharField(
+    social = models.URLField(
         verbose_name='Социальные сети',
         blank=True,
-        max_length=settings.MAX_LEN_NAME,
         null=True
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = (
+        'username',
+        'first_name',
+        'last_name',
+        'phone',
+        'city'
     )
 
     class Meta:
@@ -129,6 +130,13 @@ class User(AbstractUser):
 
 
 class Property(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Владелец недвижимости',
+        related_name='properties',
+        on_delete=models.CASCADE,
+        null=True
+    )
     name = models.CharField(
         verbose_name='Название недвижимости',
         max_length=settings.MAX_LEN_NAME
@@ -140,12 +148,11 @@ class Property(models.Model):
     worktime = models.TextField(
         verbose_name='Время работы'
     )
-    property_id = models.ForeignKey(
-        User,
-        verbose_name='Номер недвижимости',
-        related_name='properties',
-        on_delete=models.CASCADE,
-        null=True
+    area = models.FloatField(
+        verbose_name='Общая площадь помещений'
+    )
+    price = models.FloatField(
+        verbose_name='Стоимость помещений'
     )
 
     class Meta:
@@ -156,8 +163,28 @@ class Property(models.Model):
         return self.name
 
 
+class Room(models.Model):
+    property = models.ForeignKey(
+        Property,
+        verbose_name='Номер комнаты',
+        related_name='rooms',
+        on_delete=models.CASCADE,
+        null=True
+    )
+    name = models.CharField(
+        verbose_name='Название помещния',
+        max_length=settings.MAX_LEN_NAME
+    )
+    area = models.FloatField(
+        verbose_name='Площадь помещения'
+    )
+    price = models.FloatField(
+        verbose_name='Стоимость помещения'
+    )
+
+
 class Feedback_property(models.Model):
-    raiting = models.IntegerField(
+    raiting = models.PositiveSmallIntegerField(
         verbose_name='Рейтинг предприятия',
         blank=True
     )
@@ -165,14 +192,14 @@ class Feedback_property(models.Model):
         verbose_name='Текст обратной связи',
         max_length=settings.MAX_TEXT_LEN
     )
-    user_client_id = models.ForeignKey(
+    user_client = models.ForeignKey(
         User,
-        verbose_name='ФИО клиента',
+        verbose_name='Клиент',
         related_name='feedback_properties',
         on_delete=models.CASCADE,
         null=True
     )
-    feedback_id = models.ForeignKey(
+    property = models.ForeignKey(
         Property,
         verbose_name='Название недвижимости',
         related_name='feedback_properties',
--- users/validators.py
+++ users/validators.py
@@ -2,7 +2,7 @@ import re
 from string import punctuation, whitespace
 
 from django.conf import settings
-from rest_framework.validators import ValidationError
+from rest_framework import serializers
 
 
 def regex_test(value):
@@ -31,76 +31,7 @@ class CorrectUsernameAndNotMe:
     def validate_username(self, value):
         if (value == settings.NO_REGISTER_USERNAME
                 or not regex_test(value)):
-            raise ValidationError(
+            raise serializers.ValidationError(
                 f'{self.message_user}{output_incorrect_symbols(value)}'
             )
         return value.lower()
-
-
-class CorrectEmail:
-    """Проверка email на корректность."""
-    message_email = (
-        'Некоректный email, проверьте корректность написания'
-    )
-
-    def __init__(self, email):
-        self.email = email
-
-    def is_email_valid(self, email):
-        valid_chars = {'-', '_', '.'}
-        invalid_chars = set(punctuation + whitespace) - valid_chars
-        stripped_email = email.strip()
-
-        if "@" not in stripped_email:
-            return False
-
-        if stripped_email.count("@") != 1:
-            return False
-
-        local, domain = stripped_email.split("@")
-
-        for char in invalid_chars:
-            if (
-                char in local
-                and (not local.startswith('"')
-                    or not local.endswith('"'))):
-                return False
-
-        if "." in local:
-            try:
-                dot_position_in_local = local.index(".")
-
-                if local[dot_position_in_local + 1] == ".":
-                    return False
-            except:
-                return False
-
-        if local.startswith(".") or local.endswith("."):
-            return False
-
-        if domain.startswith("-") or domain.endswith("-"):
-            return False
-
-        if domain.startswith(".") or domain.endswith("."):
-            return False
-
-        dot_position_in_domain = domain.index(".")
-
-        if "." in domain and (
-            domain[
-                dot_position_in_domain
-            ] == domain[
-                dot_position_in_domain + 1
-            ]
-        ):
-            return False
-
-        return True
-
-    def validate_email(self, value):
-        if self.is_email_valid(value):
-            return value.lower()
-        else:
-            raise ValidationError(
-                f'{self.message_email}'
-            )
