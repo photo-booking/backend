1fadfad7018116d640a48dc2b42dd15e5a34a536
4aiz <milkocan@mail.ru>
2023-08-18 21:15:58 +0300

    fix some PR comments

--- photo_booking/settings.py
+++ photo_booking/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'photo_booking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
