e05cce4a8b1294061f931785dabec7e52878de7a
Geroy4ik <klevo2004@gmail.com>
2023-08-21 10:32:25 +0300

    Изменен метод create и validate_service_type

--- services/serializers.py
+++ services/serializers.py
@@ -23,39 +23,38 @@ class ServiceSerializer(serializers.ModelSerializer):
         max_length=settings.MAX_LEN_ABOUT_ME, required=False
     )
     duration = serializers.IntegerField(min_value=1, required=True)
-    service_type = PredefinedServiceTypeSerializer(read_only=True)
+    service_type = PredefinedServiceTypeSerializer()
 
     def create(self, validated_data):
         request = self.context.get('request')
         user = request.user
-        worker_service = Service.objects.create(
+        service = Service.objects.create(
             title=validated_data.get('title'),
             service_image=validated_data.get('service_image'),
             price=validated_data.get('price'),
             description=validated_data.get('description'),
             duration=validated_data.get('duration'),
-            service_type=PredefinedServiceType(
-                name=validated_data.get('service_type').get('name')
+            service_type=PredefinedServiceType.objects.get(
+                **validated_data.get('service-type')
             ),
             worker=user,
         )
-        return worker_service
+        return service
 
-    def validate_service_type(self, field):
-        service_name = field.get('name')
-        if not PredefinedServiceType.objects.filter(
-            name=service_name
-        ).exists():
+    def validate_service_type(self, service_type: PredefinedServiceType):
+        # service_name = service_type.get('name')
+        # if not PredefinedServiceType.objects.filter(
+        #     name=service_name
+        # ).exists():
+        if not PredefinedServiceType.objects.filter(**service_type).exists():
             raise serializers.ValidationError('Такого сервиса нет!', code=404)
         user = self.context.get('request').user
-        custom_type = CustomServiceType.objects.filter(
-            name=service_name
-        ).first()
+        custom_type = CustomServiceType.objects.filter(**service_type).first()
         if custom_type is not None and custom_type.worker != user:
             raise serializers.ValidationError(
                 'У Вас нет доступа к этому виду услуги', code=400
             )
-        return field
+        return service_type
 
     class Meta:
         model = Service
