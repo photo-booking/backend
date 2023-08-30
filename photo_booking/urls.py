374461598d7d57bb7c57e7685fe3c7511d045d1e
Герман Леонтьев <gerleonsto@gmail.com>
2023-08-30 23:13:03 +0300

    Добавления динамической документации API

--- photo_booking/urls.py
+++ photo_booking/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="Документация для приложения photo-market",
         # terms_of_service="URL страницы с пользовательским соглашением",
-        contact=openapi.Contact(email="admin@kittygram.ru"),
+        contact=openapi.Contact(email="german220515@yandex.ru"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
