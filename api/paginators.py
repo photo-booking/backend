353ded4337c82b6c883a5cd75dc98199706ec90f
Dzheronimo <dzheronimo@me.com>
2023-09-02 00:30:11 +1000

    Добавлен кастомный пагинатор для каталога на главной странице

--- api/paginators.py
+++ api/paginators.py
@@ -4,3 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 class LimitPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_size = 20
+
+
+class CatalogPagination(LimitPageNumberPagination):
+    page_size = 15
