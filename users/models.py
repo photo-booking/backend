1b3b231a16364e4a086f8cc4067fbb65883bfc4a
Dzheronimo <dzheronimo@me.com>
2023-09-02 00:28:55 +1000

    Поле Service убрано из модели, добавлено related_name в модель User

--- users/models.py
+++ users/models.py
@@ -7,8 +7,6 @@ from django.contrib.auth.models import (
 from django.db import models
 from phone_field import PhoneField
 
-from services.models import Service
-
 
 class AccountManager(BaseUserManager):
     def create_user(self, email, first_name='default', last_name='defaul',
@@ -67,11 +65,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone = PhoneField(
         verbose_name='Номер телефона',
         help_text='Телефон для контакта',
-    )
-    servicies = models.ManyToManyField(
-        Service,
-        verbose_name='Услуги',
-        related_name='users',
     )
     work_experience = models.FloatField(
         verbose_name='Опыт работы',
