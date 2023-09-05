d2c6874fade6ceba75b999c5f0da720ebd27519a
Vladislav Kuznetsov <108588125+VladislavCR@users.noreply.github.com>
2023-09-05 14:34:11 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -40,15 +40,10 @@ def index(request):
 
 
 class UserViewSet(DjoserUserViewSet):
-    #    queryset = User.objects.all()
     pagination_class = LimitPageNumberPagination
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['^first_name', '^last_name']
     filterset_class = UsersFilter
-
-    # def perform_create(self, serializer):
-    #     print(self.request.user)
-    #     serializer.save(user=self.request.user)
 
     def get_queryset(self):
         queryset = User.objects.all()
