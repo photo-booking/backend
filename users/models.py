2cf5277fc9f575da63ca3cba28beaca50e989b1c
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2023-09-24 13:20:52 +0300

    add custom validators for password

--- users/models.py
+++ users/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.core.validators import RegexValidator
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -76,11 +75,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     password = models.CharField(
         max_length=50,
-        validators=[RegexValidator(
-            regex=r'(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$',
-            message='Пароль должен быть минимум 8 символов\
-            и может содержать цифры,заглавные или строчные буквы')
-        ]
     )
     contact_email = models.EmailField(
         verbose_name='Почта для связи', blank=True, null=True
