dfd7fc7858a15fcabe78ca2bbb88f7196175dba0
Vladislav Kuznetsov <108588125+VladislavCR@users.noreply.github.com>
2023-10-16 18:00:48 +0300

    fixing datas for new server (#60)

--- photo_booking/settings.py
+++ photo_booking/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = [
     'localhost',
     'photo-market.acceleratorpracticum.ru',
     'backend',
+    '91.107.124.250',
+    'photomarket.sytes.net'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -202,10 +204,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
