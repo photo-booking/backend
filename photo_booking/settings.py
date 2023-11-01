7c326fcb25300ca41842eb91133e7f6430047c0e
Leontiev93 <107325873+Leontiev93@users.noreply.github.com>
2023-11-01 21:43:42 +0300

    Update settings.py

--- photo_booking/settings.py
+++ photo_booking/settings.py
@@ -200,8 +200,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
 }
 
-DJANGO_CHANNELS_REST_API = {}
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
